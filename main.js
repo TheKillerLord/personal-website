ee1c1b139a0b2abb040ac47106beb090c8309511
Louis Lim Jun Jie <louislim3101@gmail.com>
2024-05-22 23:30:56 +0100

    Added Scroll Section Active Link Setup

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*======================================Toggle Icon Navbar =================================*/
+/*====================================== Toggle Icon Navbar =================================*/
 
 
 let nemuIcon = document.querySelector('#menu-icon');
@@ -6,5 +6,37 @@ let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark');
-    navbar.classList.toggle('active')
+    navbar.classList.toggle('active');
 }
+
+
+/*====================================== Scroll Section Active Link =================================*/
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /*====================================== Sticky Navbar =================================*/
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*====================================== Remove Toggle Icon and Navbar =================================*/
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
